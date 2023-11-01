bfae84e4a4fefd76423426365e6bfaccf76b8d1f
Svin07 <svin07@ukr.net>
2023-11-01 19:14:57 +0200

    commit_01

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key = Key;
+
+  constructor(key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public person = Person;
+  public tenants: Person[];
+  public door: boolean;
+  public key = Key;
+
+  constructor(key: Key) {}
+
+  public comeIn(person: Person) {
+    if (!this.door) {
+      return;
+    }
+    this.tenants.push(person);
+  }
+
+  public abstract openDoor(arg: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(arg) {
+    if (arg === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +52,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
